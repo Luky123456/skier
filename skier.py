2aa78a1d689439be32d4f999c446e7bbbdac0ce9
Luky123456 <36738853+Luky123456@users.noreply.github.com>
2023-09-17 21:58:22 +0200

    Add files via upload

---
+++ skier.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+win = tk.Tk()
+
+w = 1000
+h = 1000
+
+canvas = tk.Canvas(width = w,height = h, bg = "black")
+canvas.pack()
+
+
+def skier(x,y,d,depth):
+    if depth > 0:
+        depth -= 1
+        canvas.create_rectangle(x, y, x+d, y+d, fill="white")
+        skier(x, y, d // 3, depth)
+        skier(x + d // 3, y, d // 3, depth)
+        skier(x + 2 * (d // 3), y, d // 3, depth)
+        skier(x, y + d // 3, d // 3, depth)
+        skier(x, y + 2 * (d // 3), d // 3, depth)
+        skier(x + d // 3, y + 2 * (d // 3), d // 3, depth)
+        skier(x + 2 * (d // 3), y + 2 * (d // 3), d // 3, depth)
+        skier(x + 2 * (d // 3), y + d // 3, d // 3, depth)
+
+skier(50, 50, 800, 6)
+
+win.mainloop()
